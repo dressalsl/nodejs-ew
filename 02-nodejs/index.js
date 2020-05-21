dfe4e2ff5d1a4329c462b3853dc4b7a20370305f
Andressa <dressalsl97@gmail.com>
2020-05-21 00:38:17 -0300

    aula 02

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -4,39 +4,98 @@
 2 obter o endereco do usuario pelo id
 */
 
-function obterUsuario(callback) {
-    seiTimeout(function () {
-        return callback(null,{
-            id: 1,
-            nome: "Andressa",
-            dataNascimento: new Date()
-        })
-    },2000);
+function obterUsuario() {
+    // quando der algum problema -> reject (ERRO)
+    // quando sucess -> RESOLV
+    return new Promise(function resolvePromise(resolve, reject) {
+        setTimeout(function () {
+
+            //return reject(new Error('DEU RUIM DE VERDADE!'))
+
+            return resolve({
+                id: 1,
+                nome: "Andressa",
+                dataNascimento: new Date()
+            })
+        },1000);
+    });
+   
 };
 
-function obterTelefone(idUsuario) {
+function obterTelefone(idUsuario, callback) {
     setTimeout(() => {
-        return {
+        return callback(null, {
             telefone: '99999999',
             ddd: 81
-        }
+        })
     }, 2000);
 
 };
 
 function obterEndereco(idUsuario) {
+    return new Promise(function resolvePromise(resolve, reject) {
+
+        setTimeout(() => {
+            return resolve({
+                rua: 'Ruralinda',
+                numero: 1
+            })
+            
+        }, 2000);
+    })
 
 };
 
-//padrao callback (erro,sucesso)
-function resolverUsuario(erro, usuario){
-    console.log('usuario', usuario);    
-};
+const usuarioPromise = obterUsuario();
 
-obterUsuario(resolverUsuario);
-//const telefone = obterTelefone(usuario.id);
+// padrao callback (erro,sucesso)
+// para manipular sucesso usamos a funcao .then
+// para manipular erros usamos o .catch
+//usuario => telefone => telefone
 
-console.log('usuario', usuario);
-//console.log('telefone', usuario);
+usuarioPromise
+    .then(function (usuario){
+        return obterTelefone(usuario.id)
+            .then(function resolverTelefone(result) {
+                return {
+                    usuario: {
+                        nome: usuario.nome,
+                        id: usuario.id
+                    },
+                    telefone: result
+                }
+            })
+    })
+    .then (function(resultado) {
+        console.log('resultado', resultado)
 
-//9:15
+    })
+    .catch (function (error) {
+        console.log('DEU RUIM', error)
+    });
+
+// obterUsuario(function resolverUsuario(error, usuario) {
+//     if (error) {
+//         console.error('DEU RUIM em USUARIO', error)
+//         return;
+//     }
+//     obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+//         if (error1) {
+//             console.error('DEU RUIM em TELEFONE', error)
+//             return;
+//         }
+//         obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+//             if (error2) {
+//                 console.error('DEU RUIM em ENDERECO', error)
+//                 return;
+//             }
+            
+//             console.log(`
+//                 Nome: ${usuario.nome},
+//                 Endereco: ${endereco.rua}, ${endereco.numero},
+//                 Telefone: ${telefone.ddd}, ${telefone.telefone}
+//             `);
+//         });
+//     })
+// });
+//12:00
